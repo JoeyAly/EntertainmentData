06cec7537fc799b08d50f096af51a52a69832459
joeyaly <joey.aly8@gmail.com>
2020-09-26 17:08:27 -0400

    Added class DVD.java

---
+++ EntertainmentData/src/DVD.java
@@ -0,0 +1,18 @@
+//This class will help print out information when a DVD is called in database
+public class DVD extends Item {
+	
+	private String director;
+	
+	//Takes in variables from the Database class and saves it
+	//Allows it to be formated and printed as a result
+	public DVD(String title, String director, int playingTime) {
+		
+		super(title, playingTime);
+		this.director = director;
+			}
+	//Return directors name
+	public String getDirector() {
+		return director;
+	}
+	
+}
