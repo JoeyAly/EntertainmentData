5e9208ee633e91fc4e507d91a8dbc37be59f51e7
joeyaly <joey.aly8@gmail.com>
2020-09-26 14:33:51 -0400

    Added class Item.java

---
+++ EntertainmentData/src/Item.java
@@ -0,0 +1,47 @@
+//This class gets inherited from DVD and CD classes
+public class Item {
+	
+	private String comment;
+	private boolean gotIt;
+	private int playingTime;
+	private String title;
+  
+//Default class that other classes inherits from	
+	public Item(String title, int playingTime) {
+		this.title = title;
+		this.playingTime = playingTime;
+		this.gotIt = false;
+		this.comment = null;
+	}
+
+	//returns comment
+	public String getComment() {
+		return comment;
+	}
+	//returns if owned
+	public boolean getOwn() {
+		return gotIt;
+	}
+
+
+	public void print() {
+		System.out.println("Print statement");
+	}
+	
+	//Sets comment
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	//Sets if owned
+	public void setOwn(boolean gotIt) {
+		this.gotIt = gotIt;
+	}
+	
+	//Sets format for CD and DVD class
+//	public String toString() {
+//		
+//	}
+//	
+}
+	
